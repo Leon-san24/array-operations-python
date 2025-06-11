9569b82a4f4422b389679c228db33afd1a2cddac
Leon Blyum <leonblyum@MacBook-Pro-von-Leon.local>
2025-06-11 09:23:26 +0200

    added docs

--- algorithms/sort_biggest.py
+++ algorithms/sort_biggest.py
@@ -6,11 +6,17 @@ Author: Leon Blyum
 """
 
 def sort_biggest(pList: list) -> int:
+    """ Returns the biggest number as a int from a list.
+
+    Args:
+        pList (list): Input list.
+
+    Returns:
+        int: Biggest number of the list.
+    """
     biggest_int = None
     while pList:
         if biggest_int == None or biggest_int < pList[0]:
             biggest_int = pList[0]
         pList.pop(0)
     return biggest_int
-
-
--- algorithms/sort_smallest.py
+++ algorithms/sort_smallest.py
@@ -6,6 +6,14 @@ Author: Leon Blyum
 """
 
 def sort_smallest(pList: list) -> int:
+    """ Returns the smallest number as a int from a list.
+
+    Args:
+        pList (list): Input list.
+
+    Returns:
+        int: Smallest number of the list.
+    """
     smallest_int = None
     while pList:
         if smallest_int == None or smallest_int > pList[0]:
