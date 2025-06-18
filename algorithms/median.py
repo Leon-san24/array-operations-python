e374c4759053301fdf48db2bbed19c49c6d6d53c
Tom Fischer <tomfi2003@gmail.com>
2025-06-18 08:34:50 +0200

    Added documentation

--- algorithms/median.py
+++ algorithms/median.py
@@ -1,6 +1,20 @@
+"""
+Task number five, build a function to find the median using the built in sort function.
+
+Author: Tom Fischer
+
+"""
 def median (pList: list) -> int:
+    """ Returns the median number of a list as a float.
+
+    Args:
+        pList (list): Input list.
+
+    Returns:
+        float: Median number of the list.
+    """
     pList.sort()
     if len(pList) % 2 == 0:
-        return (pList[int(len(pList)/2)]+pList[int(len(pList)/2-1)])/2
+        return float((pList[int(len(pList)/2)]+pList[int(len(pList)/2-1)])/2)
     else:
-        return pList[int(len(pList)/2-0.5)]
+        return float(pList[int(len(pList)/2-0.5)])
